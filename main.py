e55bbd24787edecfce9a5cd4497dc5eef6d9bb9b
Max <max.bogatec@gmail.com>
2020-10-27 05:04:23 +0100

    insert statement
    updated insert statement for more performance (10000 combos in <1s)

--- main.py
+++ main.py
@@ -20,18 +20,22 @@ class insertToDb(threading.Thread):
         threading.Thread.__init__(self)
         self.id=id
         self.data=data
-
+#Threadding
     def run(self):
         print(f"Starting Thread {self.id} for Insert")
-        print(self.data)
+
+        query = self.data
+
+        stmt = "INSERT INTO data (identifier, authenticator) VALUES (%s, %s)"
         try:
             db_config = read_db_config()
             conn = MySQLConnection(**db_config)
 
             cursor = conn.cursor()
-            cursor.execute(self.data,multi=True)
+            cursor.executemany(stmt,query)
 
             conn.commit()
+            print("Insert successfull")
         except Error as error:
             print(error)
 
@@ -45,25 +49,25 @@ def getData(target,seperator):
     file = open(target,"r")
 
     with file as t_file:
-        querryString = ""
+        querryString = []
         resetTicker = 0
         for line in t_file:
             stripped = line.strip()
             splitted = stripped.split(seperator)
-            querryString +=f"INSERT INTO data (identifier,authenticator) VALUES ('{splitted[0]}','{splitted[1]}');"
+            querryString.append((splitted[0],splitted[1]))
             resetTicker +=1
             if resetTicker >= max_inserts:
                 t1 = insertToDb(1,querryString)
                 t1.start()
                 resetTicker = 0
-                querryString = ""
-                time.sleep(2.4)
+                querryString = []
+                #time.sleep(2.4)
     #If data remains push it to the thread aswell
     if querryString != "":
         t1 = insertToDb(1,querryString)
         t1.start()
         resetTicker = 0
-        querryString = ""
+        querryString = []
     
     file.close()
     #print(querryString)
